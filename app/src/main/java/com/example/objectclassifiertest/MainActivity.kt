cc1c38165e5a466a82c5a887abd477e48014281e
Sanggeon Park <gunnih@gmail.com>
2020-10-07 15:31:41 +0200

    asking camera permission.

--- app/src/main/java/com/example/objectclassifiertest/MainActivity.kt
+++ app/src/main/java/com/example/objectclassifiertest/MainActivity.kt
@@ -1,11 +1,57 @@
 package com.example.objectclassifiertest
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    private val permissions = listOf(Manifest.permission.CAMERA)
+    private val permissionsRequestCode = Random.nextInt(0, 10000)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (!hasCamera(this)) {
+            return
+        }
+        if (hasPermissions(this)) {
+            setupCameraPreview()
+        } else {
+            ActivityCompat.requestPermissions(
+                this, permissions.toTypedArray(), permissionsRequestCode
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<out String>,
+                                            grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == permissionsRequestCode && hasPermissions(this)) {
+            setupCameraPreview()
+        } else {
+            finish() // If we don't have the required permissions, we can't run
+        }
+    }
+
+    private fun setupCameraPreview() {
+
+    }
+
+    private fun hasCamera(context: Context): Boolean {
+        return context.packageManager.hasSystemFeature(PackageManager.FEATURE_CAMERA)
+    }
+
+    private fun hasPermissions(context: Context) = permissions.all {
+        ContextCompat.checkSelfPermission(context, it) == PackageManager.PERMISSION_GRANTED
     }
 }
